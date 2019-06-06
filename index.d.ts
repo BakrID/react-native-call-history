c323070b964691bf4cbfd1c198d0825526e9b911
George <bakrid@outlook.com>
2019-06-06 11:40:31 +0300

    fix d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
 declare module 'react-native-call-history' {
-	export function getCallHistory(date: string): Promise<{number: string, type: string, date: string, rawDate: string, duration: string}[]>;
+	export function getCallHistory(startDate: string, endDate: string): Promise<{number: string, type: string, date: string, rawDate: string, duration: string}[]>;
 }
